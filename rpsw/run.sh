0d5818cd2677adbe4ac9484f822f1e61cc65d5fa
yalidu <yali.du86@gmail.com>
2021-08-01 09:59:28 +0200

    clean

--- rpsw/run.sh
+++
@@ -1,3 +0,0 @@
-python3 main.py -num_worker -1 -game matrix_game -output_path /Users/yalidu/Projects/working/Archive-2/res/ -sig_size 0 -independent 1
-
-python3 main.py -num_worker -1 -game matrix_game -output_path /Users/lhchen/Desktop/ -sig_size 0 -independent 1
